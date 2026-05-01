ec92b5c4bc6fdf1aff290537e59eb7ba5251ea3f
PriyanshiBajpai09 <priyanshi2004bajpai@gmail.com>
2026-05-01 16:38:02 +0530

    fix backend port

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("DB connected"))
-.catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("API running 🚀");
@@ -24,4 +25,8 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on", PORT);
+});
