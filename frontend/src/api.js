c357fc79a737e75fb4d5bbcb0a0edfb5e4f70e95
PriyanshiBajpai09 <priyanshi2004bajpai@gmail.com>
2026-05-01 17:49:12 +0530

    update url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskmanagerassignment-production-ad45.up.railway.app/",
+  baseURL: "https://taskmanagerassignment-production-0a05.up.railway.app/api",
 });
 
 API.interceptors.request.use((req) => {
