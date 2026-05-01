e43fd6db9dfb412e13934cb7c70e10a195c19097
PriyanshiBajpai09 <priyanshi2004bajpai@gmail.com>
2026-05-01 17:22:21 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,6 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on", PORT);
+app.listen(process.env.PORT, () => {
+  console.log("Server running");
 });
